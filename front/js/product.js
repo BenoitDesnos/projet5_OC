03d9b86c22a545f9d2313e4e3f35800136ad78dd
desnos <benoit.desnos66@gmail.com>
2022-08-17 13:53:11 +0200

    w3c

--- front/js/product.js
+++ front/js/product.js
@@ -3,40 +3,80 @@ const title = document.getElementById("title");
 const price = document.getElementById("price");
 const description = document.getElementById("description");
 const colors = document.getElementById("colors");
+const quantity = document.getElementById("quantity");
 
 let addedToCart = [];
-let addedToCart2 = [];
-function addToStorage() {
-  console.log(addedToCart[2]);
-  if (addedToCart[2] !== colors.value || addedToCart[2] !== undefined) {
-    console.log("test");
-    addedToCart2 = [
-      (id = checkIdUrl()),
-      (quantity = quantity.value),
-      (color = colors.value),
-    ];
-    let stringifiedTocart = JSON.stringify(addedToCart2);
-    localStorage.setItem("test", stringifiedTocart);
-    console.log(addedToCart2);
+let count = 0;
+let colorWatch = "";
+let randomIds = [];
+
+// crée un random id et le stock dans l'array randomIds, rejoue la fonction si id existe dans randomIds
+function makeId() {
+  let length = 20;
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  console.log("test2");
-  addedToCart = [
-    (id = checkIdUrl()),
-    (quantity = quantity.value),
-    (color = colors.value),
-  ];
-  let stringifiedTocart = JSON.stringify(addedToCart);
-  localStorage.setItem(checkIdUrl(), stringifiedTocart);
+  if (randomIds.includes(result)) {
+    makeId();
+  }
+  randomIds.push(result);
+  console.log(randomIds);
+  return result;
 }
 
+// récupère la valeur de l'input de la couleur de l'article
+const colorsCount = () => {
+  colorWatch = colors.value;
+};
+
+/* colors.addEventListener("input", colorsCount); */
+
+// récupère la valeur de l'input du nombre d'articles
+const articlesCount = () => {
+  count = quantity.value;
+};
+
+/* quantity.addEventListener("input", articlesCount); */
+
+// ajoute au localstorage un id, nombre d'articles et la couleur
+function addToStorage() {
+  articlesCount();
+  /*  console.log(count + " count");
+  console.log(addedToCart[1] + " cartCount"); */
+  colorsCount();
+  /*   console.log(colorWatch + " colorsWatch");
+  console.log(addedToCart[2] + " colorCart"); */
+
+  //verifie si meme couleur et nombre d'article differents, si oui change juste le nombre d'articles et modifie le nombre d'articles dans le dernier id dans le localstorage
+  if (addedToCart[2] === colorWatch && count !== addedToCart[1]) {
+    addedToCart[1] = count;
+    console.log(localStorage.key(0));
+    let stringifiedTocart = JSON.stringify(addedToCart);
+    localStorage.setItem(randomIds[randomIds.length - 1], stringifiedTocart);
+    // verifie si couleur et nmbres d'articles pareil, si oui message le précisant
+  } else if (addedToCart[2] === colorWatch && count === addedToCart[1]) {
+    alert("Objet déjà présent dans le panier");
+    // si aucun des cas du dessus alors crée id et sotck dans le local storage
+  } else {
+    addedToCart = [checkIdUrl(), count, colorWatch];
+    let stringifiedTocart = JSON.stringify(addedToCart);
+    localStorage.setItem(makeId(), stringifiedTocart);
+  }
+}
 addToCart.addEventListener("click", addToStorage);
 
+// récupère le paramètre id dans l'url
 const checkIdUrl = () => {
   const params = new URLSearchParams(window.location.search);
   const idParams = params.get("id");
   return idParams;
 };
 
+// récupère la data de l'api à l'aide de l'id dans l'url
 const retrieveProductsData = () =>
   fetch("http://localhost:3000/api/products/" + checkIdUrl())
     .then((res) => res.json())
@@ -48,10 +88,10 @@ const retrieveProductsData = () =>
       )
     );
 
+// les deux fonctions suivantes remplissent le html à l'aide de la fonction mainProduct en dessous qui récupère la data en asynchrone
 const fillItemImg = (product) => {
   itemImg.innerHTML = `<img src="${product.imageUrl}" alt="Photographie d'un canapé"></img>`;
 };
-
 const fillItemContent = (product) => {
   title.textContent = product.name;
   price.textContent = product.price;
@@ -62,10 +102,10 @@ const fillItemContent = (product) => {
   });
 };
 
+// récupère la data en asynchrone
 const mainProduct = async () => {
   const productData = await retrieveProductsData();
   console.log(productData);
-
   fillItemImg(productData);
   fillItemContent(productData);
 };
